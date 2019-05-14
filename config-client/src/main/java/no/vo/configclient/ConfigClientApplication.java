7ba268b198dc4cfb2e6129b96473af40b1fdc86d
newisaboy <975220638@qq.com>
2019-05-14 23:04:03 +0800

    1111

--- config-client/src/main/java/no/vo/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/no/vo/configclient/ConfigClientApplication.java
@@ -2,7 +2,11 @@ package no.vo.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableDiscoveryClient
+@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
 @SpringBootApplication
 public class ConfigClientApplication {
 
