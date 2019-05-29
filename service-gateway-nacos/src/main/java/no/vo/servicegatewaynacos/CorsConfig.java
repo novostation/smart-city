ed931059df6869efd6f20b53ce2cdf035a527381
novostation <975220638@qq.com>
2019-05-29 19:53:22 +0800

    111111111

--- service-gateway-nacos/src/main/java/no/vo/servicegatewaynacos/CorsConfig.java
+++ service-gateway-nacos/src/main/java/no/vo/servicegatewaynacos/CorsConfig.java
@@ -1,47 +1,46 @@
 package no.vo.servicegatewaynacos;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.util.pattern.PathPatternParser;
 import reactor.core.publisher.Mono;
 
-@EnableDiscoveryClient
-@SpringBootApplication
-public class ServiceGatewayNacosApplication {
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServiceGatewayNacosApplication.class, args);
-    }
+/**
+ * description:
+ *
+ * @author wangpeng
+ * @date 2019/01/02
+ */
+@Configuration
+public class CorsConfig {
+    /*@Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
 
+        return new CorsWebFilter(source);
+    }*/
 
-    private static final String ALL = "*";
-    private static final String MAX_AGE = "18000L";
-    @Bean
-    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient,
-                                                                        DiscoveryLocatorProperties properties) {
-        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
-    }
-    @Bean
-    public ServerCodecConfigurer serverCodecConfigurer() {
-        return new DefaultServerCodecConfigurer();
-    }
+   /* private static final String MAX_AGE = "18000L";
     @Bean
     public WebFilter corsFilter() {
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
@@ -67,6 +66,23 @@ public class ServiceGatewayNacosApplication {
             }
             return chain.filter(ctx);
         };
+    }*/
+
+   /* @Bean
+    public CorsWebFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsWebFilter(source);
     }
 
+    private CorsConfiguration buildConfig(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //在生产环境上最好指定域名，以免产生跨域安全问题
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }*/
+
 }
+
